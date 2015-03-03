51646958e5eb8a1a0b58d7ccd1476df9f5d651bf
Andrew Krock <ajkrock93@aol.com>
2015-03-03 10:52:28 -0500

    first commit of tiny shell

---
+++ shell.cpp
@@ -0,0 +1,14 @@
+/*  Author: Andrew Krock
+    Name: shell.cpp
+    Description: A tiny shell
+    Last Edit: 02/18/15
+*/
+
+#include <iostream>
+#include <string>
+
+int main(){
+    char name;
+    std::cin.getline(name)
+    std::cout<<name;
+}
